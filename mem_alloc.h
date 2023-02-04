71796e6c28cd2903fd822e778b17fefbb07a0d35
MoNam97 <mohamad0namdar@gmail.com>
2023-02-04 11:02:44 +0330

    two allocation algorithms merged :)

--- mem_alloc.h
+++ mem_alloc.h
@@ -9,7 +9,7 @@
 
 #define MetaDataSize 40
 
-char error_message[100];
+extern char mem_alloc_error_message[];
 
 typedef struct MetaData {
     struct MetaData *next;
@@ -32,5 +32,29 @@ void my_free(void *block_ptr);
 void *my_realloc(void *block_ptr, size_t size, char fill);
 
 void show_stats();
+
+///////////////////////// FIRST FIT /////////////////////////
+
+#define BUDDY_MAX_ORDER 30
+#define BUDDY_MIN_BLOCK_SIZE 32
+#define BUDDY_MIN_ORDER 5
+
+#define BUDDY_BLOCK_HEADER_SIZE 24
+
+typedef struct block_header {
+    char is_free;
+    size_t size;
+    struct block_header *next;
+} block_header;
+
+int buddy_variable_init(int max_order, size_t memory_size, void *memory_start);
+
+void * mem_alloc_buddy(size_t size, char fill);
+
+void free_buddy(void *block_ptr);
+
+void show_buddy_memory();
+
+///////////////////////// BUDDY MEMORY /////////////////////////
 
 #endif
