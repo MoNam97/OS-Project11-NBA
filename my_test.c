22134cd34e53fc3c079181e1d58c10286c1de7b4
AmiraliE1380 <ebrahimzadeh.amirali@gmail.com>
2023-02-04 18:18:03 +0330

    Minor changes in my_test

--- my_test.c
+++ my_test.c
@@ -15,10 +15,10 @@ int main(int argc, char **argv) {
   set_minimum_size(MIN_SIZE / 4);
 
   char *dummy = my_malloc(10, 'z');
-  prinf("%Testing allocation less than minimum size: %s\n", dummy);
+  printf("Testing allocation less than minimum size: %s\n", dummy);
 
   dummy = my_malloc(20000, 'z');
-  prinf("%Testing allocation more than maximum size: %s\n", dummy);
+  printf("Testing allocation more than maximum size: %s\n", dummy);
 
   char *str = my_malloc(100, '8');
   str[99] = '\0';
@@ -32,13 +32,14 @@ int main(int argc, char **argv) {
   my_free(str);
   printf("%s\n", str);
 
-  size_t size = 20
+  size_t size = 20;
+  int *arr;
   arr = my_malloc(size * sizeof(int), 'z');
   printf("\n");
   for(int i = 0; i < size; i++) 
     arr[i] = i;
   for(int i = 0; i < size; i++) 
-    print("%p:\t%d\n", &arr[i], i);
+    printf("%p:\t%d\n", &arr[i], i);
   
   size *= 2;
   arr = my_realloc(arr, size * sizeof(int), 'z');
@@ -46,15 +47,15 @@ int main(int argc, char **argv) {
   for(int i = 0; i < size; i++) 
     arr[i] = i;
   for(int i = 0; i < size; i++) 
-    print("%p:\t%d\n", &arr[i], i);
+    printf("%p:\t%d\n", &arr[i], i);
 
   arr = my_realloc(arr, 0, 'z');
-  printf("%s", arr);
+  printf("%s", (char *)arr);
 
   arr = my_realloc(arr, 20, 'z');
   arr[18] = '\n';
   arr[19] = '\0';
-  printf("%s", arr);
+  printf("%s", (char *)arr);
   
   return 0;
 }
